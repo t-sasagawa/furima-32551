15768a537142dea68391954f56d757247ba06f07
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2020-12-25 14:39:46 +0900

    新規登録/ログイン/ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+       
+  validates :nickname,        presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :last_name#, presence: true
+    validates :first_name#, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
+    validates :last_name_kana#, presence: true
+    validates :first_name_kana#, presence: true
+  end
+  validates :birthday,        presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  # LAST_NAME_KANA_REGEX = /^([ァ-ン]|ー)+$/.freeze
+  # validates_format_of :last_name_kana, with: LAST_NAME_KANA_REGEX, message: 'Full-width katakana characters'
+
 end
