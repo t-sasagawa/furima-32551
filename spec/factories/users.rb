8841f86685b76d7d6a6b1a25f76660c0dfedbc4c
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2020-12-26 22:04:32 +0900

    FactoryBotの設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"Aあア漢字"}
+    email                 {Faker::Internet.free_email}
+    password              {"1a"+Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"あア漢字"}
+    first_name            {"あア漢字"}
+    last_name_kana        {Gimei.last.katakana}
+    first_name_kana       {Gimei.first.katakana}
+    birthday              {Faker::Date.between_except(from: '1930-1-1', to: 5.year.ago, excepted: Date.today)}
+  end
+end
