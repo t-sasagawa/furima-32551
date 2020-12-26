fd4e1073769f3775b3d6b6becffde23adbf995d6
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2020-12-26 22:05:31 +0900

    ユーザーモデルのバリデーション（アソシエーションなし）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,16 @@ class User < ApplicationRecord
        
   validates :nickname,        presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
-    validates :last_name#, presence: true
-    validates :first_name#, presence: true
+    validates :last_name
+    validates :first_name
   end
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
-    validates :last_name_kana#, presence: true
-    validates :first_name_kana#, presence: true
+    validates :last_name_kana
+    validates :first_name_kana
   end
   validates :birthday,        presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
-  # LAST_NAME_KANA_REGEX = /^([ァ-ン]|ー)+$/.freeze
-  # validates_format_of :last_name_kana, with: LAST_NAME_KANA_REGEX, message: 'Full-width katakana characters'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
 end
