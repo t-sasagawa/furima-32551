ae01c92f08786d90c60ff7c2d64647b4c3f7381b
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2020-12-30 14:08:05 +0900

    FactoryBot修正 / テスト画像追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    title         {"あア漢字"}
+    text          {"あア漢字"}
+    category_id   {1}
+    state_id      {2}
+    fee_id        {1}
+    prefecture_id {2}
+    days_ship_id  {1}
+    price         {345}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/images/test.jpg'), filename: 'test.jpg')
+    end
   end
+
 end
