07949ced4ec1a859bd6aa13877b9eebfbb1ca3a6
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-11 19:06:12 +0900

    FactoryBot定義

---
+++ spec/factories/shipping_orders.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :shipping_order do
+    postal_code   {"111-1111"}
+    prefecture_id {1}
+    municipality  {"あア漢字"}
+    address       {"あア漢字１"}
+    building {"あア漢字１"}
+    phone_number {"09012345678"}
+
+
+  end
+end
