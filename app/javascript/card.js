d1554db001a7c219442f7ce5e6298cd00d8f46ef
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-12 12:08:24 +0900

    テスト公開鍵仮設置

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,10 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵(仮設置)
   // console.log("カード情報トークン化のためのJavaScript");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    // console.log("フォーム送信時にイベント発火")
   });
 };
 
