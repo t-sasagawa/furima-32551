b2c1c726a7a6755c3d9f8b6e6b89b2ba84b56c12
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-12 18:00:04 +0900

    サーバーサイドにフォーム情報を送信

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -31,7 +31,7 @@ const pay = () => {
       document.getElementById("order_exp_month").removeAttribute("name")
       document.getElementById("order_exp_year").removeAttribute("name")
 
-
+      document.getElementById("charge-form").submit();
 
     });
 
