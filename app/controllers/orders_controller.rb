5ed99bc4b233080fae76fc8a7d2ad9c803c761e9
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-12 17:25:26 +0900

    位置ずれ修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,15 +19,20 @@ class OrdersController < ApplicationController
     end
   end
 
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  end
+  
 
-private
+  private
 
-  def order_params
-    params.require(:shipping_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-  end
+    def order_params
+      params.require(:shipping_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
 
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: order_params[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      )      
+    end
 end
-
