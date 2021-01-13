a315f8c4fc05e2919685e6ac0131922bcf4cc680
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-13 14:29:07 +0900

    バリデーションtoken追加

--- app/models/shipping_order.rb
+++ app/models/shipping_order.rb
@@ -1,9 +1,10 @@
 class ShippingOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :user_id, :item_id, :token
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
+    validates :token
     validates :postal_code,          format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }            #郵便番号
     validates :municipality #市区町村
     validates :address #番地
