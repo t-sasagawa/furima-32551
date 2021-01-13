fc826c87796175191ab20f32c62208ede1481cb7
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-13 17:19:58 +0900

    バリデーションの追加

--- app/models/shipping_order.rb
+++ app/models/shipping_order.rb
@@ -4,6 +4,8 @@ class ShippingOrder
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :token
     validates :postal_code,          format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }            #郵便番号
     validates :municipality #市区町村
